aca092d6d940ac46bf7daa766f6cf449e3f0f9d6
Sam Eakin <sseakin@gmail.com>
2017-04-28 08:39:51 -0500

    randomization of points

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@
 
 import java.awt.Graphics;
 import javax.swing.JFrame;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main extends JFrame {
 	public Main() {
@@ -14,12 +15,29 @@ public class Main extends JFrame {
 	}
 
 	public void paint(Graphics g) {
-		Point a = new Point(250,250);
-		Point b = new Point(400,100);
-		Point c = new Point(400,400);
+		Point a = new Point(250, 250);
+		Point b = new Point(400, 100);
+		Point c = new Point(400, 400);
 		g.drawLine(a.x, a.x, b.x, b.y);
 		g.drawLine(c.x, c.x, b.x, b.y);
 		g.drawLine(a.x, a.x, c.x, c.y);
+
+		Point tracer = new Point(10, 10);
+		
+		/* TODO: 
+		 * first just do a static fractal generation of a large number of pixels drawn.
+		 * 1. create 3 target points (a,b,c)
+		 * 2. create random point
+		 * 3. for 2000 iterations
+		 *       roll random number
+		 *       if ( 1|2 - a, 3|4 - b, 5|6 - c)
+		 *           move half the distance in the direction towards the according point
+		 *           draw new point
+		 */
+		for (int i=0; i < 1000; i++) {
+			g.drawLine(tracer.x, tracer.y, tracer.x, tracer.y);
+			tracer.randomize();
+		}
 	}
 
 	private void setPoint() {
@@ -39,4 +57,11 @@ class Point {
 		this.x = x;
 		this.y = y;
 	}
+	public void randomize() {
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		x = ThreadLocalRandom.current().nextInt(0, 499 + 1);
+		y = ThreadLocalRandom.current().nextInt(0, 499 + 1);
+	}
+
 }
