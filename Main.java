c947a8fb45cf4a0970d66f29300b7fa1b4bf2e2a
Sam Eakin <sseakin@gmail.com>
2017-04-27 23:12:15 -0500

    Initializing repo

---
+++ Main.java
@@ -0,0 +1,15 @@
+/** Main.java
+ * 	Chaos Game Fractal Visualization
+ */
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Fractal");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
