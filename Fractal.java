b2d14563cabac9573d08e9ff661a5023e32107b5
Sam Eakin <sseakin@gmail.com>
2017-04-28 11:36:16 -0500

    Run to draw 3 fractals to screen

--- Fractal.java
+++ Fractal.java
@@ -1,15 +1,17 @@
 /** Fractal.java
  * 	Chaos Game Fractal Visualization
  */
+import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Fractal extends JFrame {
 	public Fractal() {
-		setTitle("Fractal");
+		int size = 800;
+		setTitle("Chaos Game Fractals");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,500);
+		setSize(800,800);
 		setVisible(true);
 	}
 
@@ -18,25 +20,30 @@ public class Fractal extends JFrame {
 	}
 
 	public void paint(Graphics g) {
-		// draw triangle
-		Point a = new Point(250, 100);
-		Point b = new Point(100, 100);
-		Point c = new Point(400, 400);
-		g.drawLine(a.x, a.x, b.x, b.y);
-		g.drawLine(c.x, c.x, b.x, b.y);
-		g.drawLine(a.x, a.x, c.x, c.y);
+		int fractals = 3;
+		for (int i=0; i < fractals; i++) {
+			// draw random triangle
+			Triangle randomTri = new Triangle();
 
-		Point tracer = new Point(10, 10);
-		
-		for (int i=0; i < 10000; i++) {
+			// draw random tracer point and define number of iterations to run
+			Point tracer = new Point();
+			int iterations = 20000;
+
+			redraw(g, randomTri, tracer, iterations);
+		}
+	}
+
+
+	public void redraw(Graphics g, Triangle tri, Point tracer, int iterations) {
+		for (int i=0; i < iterations; i++) {
 			g.drawLine(tracer.x, tracer.y, tracer.x, tracer.y);
 			int roll = rollDice();
 			if (roll==1 || roll==2)
-				setPoint(tracer, a);
+				setPoint(tracer, tri.a);
 			else if (roll==3 || roll==4)
-				setPoint(tracer, b);
+				setPoint(tracer, tri.b);
 			else if (roll==5 || roll==6)
-				setPoint(tracer, c);
+				setPoint(tracer, tri.c);
 		}
 	}
 
@@ -53,6 +60,11 @@ class Point {
 	int x;
 	int y;
 
+	Point() {
+		x = ThreadLocalRandom.current().nextInt(0, 799 + 1);
+		y = ThreadLocalRandom.current().nextInt(0, 799 + 1);
+	}
+
 	Point(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -64,10 +76,17 @@ class Point {
 		this.x = midX;
 		this.y = midY;
 	}
+}
 
-	public void randomize() {
-		x = ThreadLocalRandom.current().nextInt(0, 499 + 1);
-		y = ThreadLocalRandom.current().nextInt(0, 499 + 1);
+class Triangle {
+	Point a;
+	Point b;
+	Point c;
+
+	Triangle() {
+		this.a = new Point();
+		this.b = new Point();
+		this.c = new Point();
 	}
 }
 
